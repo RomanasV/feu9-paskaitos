83ddcce54f46fdcf1797835d37b0b1e8a4ad67c5
RomanasV <romanasvenckus@gmail.com>
2023-09-04 13:29:47 +0300

    js basics tasks

---
+++ 2023-09-04-js-basics/task.js
@@ -0,0 +1,28 @@
+// SLAPTAŽODIS
+let password = 'fsjklskldfjshf'
+
+// AMŽIUS
+// let age = prompt('Iveskite savo amziu')
+let age = 4
+console.log(age)
+
+
+// ŽAIDIMAS
+
+// Artimiausia nuo Žemės esanti Žvaigždė?
+let correctAnswer11 = 'sun'
+let playerAnswer11 = 'sun'
+
+// 123 + 456 = 579
+let correctAnswer12 = 579
+let playerAnswer12 = 4546
+
+console.log(correctAnswer11 === playerAnswer11 && correctAnswer12 === playerAnswer12)
+console.log(correctAnswer11 === playerAnswer11 || correctAnswer12 === playerAnswer12)
+
+
+// PASISVEIKINIMAS
+let isLoggedIn = false
+let userName = 'John'
+let time = 16
+let isBirthday = false

dbde80d58baf85b6a0bea6fc71826cb7f69f97c0
RomanasV <romanasvenckus@gmail.com>
2023-09-06 10:49:59 +0300

    tasks for functions

---
+++ 2023-09-05-functions/task.js
@@ -0,0 +1,12 @@
+// 1. Funkciją, kuri paverčia žmogaus metus į šuns metus:
+// 1.1. Funkcija priima vieną parametrą: žmogaus amžius.
+// 1.2. Funkcija grąžina atsakymą tokiu formatu: 5 žmogaus metai yra 35 šuns metai.
+
+function humanToDogYears(humanYears) {
+  let dogYears = humanYears * 7
+  let output = `${humanYears} žmogaus metai yra ${dogYears} šuns metai.`
+  return output
+}
+
+console.log(humanToDogYears(25))
+console.log(humanToDogYears(10))

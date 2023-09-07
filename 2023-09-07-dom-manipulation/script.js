6805fe883570477d71bb8c065fcd476bb1cc894f
RomanasV <romanasvenckus@gmail.com>
2023-09-07 13:59:33 +0300

    dom manipulation

---
+++ 2023-09-07-dom-manipulation/script.js
@@ -0,0 +1,6 @@
+// Paselektinti HTML elementą
+let paragraph = document.querySelector('p')
+paragraph.textContent = 'Sveikas'
+paragraph.innerHTML = 'Labas <span style="color: red;">vakaras</span>'
+
+

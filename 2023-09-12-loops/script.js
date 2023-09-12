303fa9d1a72d9dcaf77eb8915a414d218af4cad4
RomanasV <romanasvenckus@gmail.com>
2023-09-12 14:38:44 +0300

    foor loop starter

---
+++ 2023-09-12-loops/script.js
@@ -0,0 +1,85 @@
+// console.log('- ' + 1)
+// console.log('- ' + 2)
+// console.log('- ' + 3)
+// console.log('- ' + 4)
+// console.log('- ' + 5)
+// console.log('- ' + 6)
+// console.log('- ' + 7)
+// console.log('- ' + 8)
+// console.log('- ' + 9)
+// console.log('- ' + 10)
+// console.log('- ' + 11)
+// console.log('- ' + 12)
+// console.log('- ' + 13)
+// console.log('- ' + 14)
+// console.log('- ' + 15)
+// console.log('- ' + 16)
+// console.log('- ' + 17)
+// console.log('- ' + 18)
+// console.log('- ' + 19)
+// console.log('- ' + 20)
+
+function count(num) {
+  console.log('- ' + num + ' !!!')
+}
+
+// count(1)
+// count(2)
+// count(3)
+// count(4)
+// count(5)
+// count(6)
+// count(7)
+// count(8)
+// count(9)
+// count(10)
+// count(11)
+// count(12)
+// count(13)
+// count(14)
+// count(15)
+// count(16)
+// count(17)
+// count(18)
+// count(19)
+// count(20)
+
+
+
+// FOR CIKLAS (LOOP)
+// 1. Iniciavimo žodelis - for
+// 2. Paprasti skliaustai - ()
+// 2.1. Sukuriamas kintamasis (dažniausiai jo pavadinimas yra i)
+// 2.2. Sąlyga
+// 2.3. Kintamojo vertės keitimas
+// 3. Riestiniai skliaustai - {}
+
+/*
+
+  for (kintamasis; sąlyga; kintamojo keitimas) {
+
+  }
+
+*/
+
+// for (let i = 1; i <= 20; i++) {
+//   console.log(i)
+// }
+
+// for (let i = 0; i < 20; i++) {
+//   console.log(i)
+// }
+
+
+// for (let i = 1; i <= 20; i++) {
+//   count(i)
+// }
+
+
+function task1() {
+  for (let i = 1; i <= 100; i++) {
+    console.log(i)
+  } 
+}
+
+// task1()

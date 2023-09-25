e2dbd7c8397052397835d6283e3fac163d6e639c
RomanasV <romanasvenckus@gmail.com>
2023-09-25 10:01:36 +0300

    form created

--- 2023-09-21-forms/script.js
+++ 2023-09-21-forms/script.js
@@ -1,24 +1,59 @@
 function init() {
-  const testForm = document.querySelector('#test-form')
+  const studentForm = document.querySelector('#student-form')
 
-  testForm.addEventListener('submit', (event) => {
+  studentForm.addEventListener('submit', (event) => {
     event.preventDefault()
 
-    // const personName = document.querySelector('#person-name').value
-    // const personName = document.getElementById('person-name').value
-    // const personName = testForm.querySelector('#person-name').value
+    const form = event.target
 
-    // const personAge = testForm.elements.age.value
-    // const personAge = testForm.elements['person-age'].value
-    
-    // const personAge = testForm['person-age'].value
-    const personAge = testForm.age.value
-    const personName = testForm.name.value
+    const name = form.name.value
+    const surname = form.surname.value
+    const age = form.age.value
+    const phone = form.phone.value
+    const email = form.email.value
+    const itKnowledge = form.elements['it-knowledge'].value
+    const group = form.group.value
+    const interests = form.querySelectorAll('input[name="interest"]:checked')
 
-    const personDescription = document.createElement('p')
-    personDescription.textContent = `Person name is ${personName} and age is ${personAge}`
+    const studentsList = document.querySelector('#students-list')
 
-    testForm.after(personDescription)
+    const studentItem = document.createElement('div')
+    studentItem.classList.add('student-item')
+
+    const nameElement = document.createElement('h2')
+    nameElement.textContent = `${name} ${surname}`
+
+    const ageElement = document.createElement('p')
+    ageElement.innerHTML = `<strong>Age:</strong> ${age}`
+
+    const phoneElement = document.createElement('p')
+    phoneElement.innerHTML = `<strong>Phone:</strong> ${phone}`
+
+    const emailElement = document.createElement('p')
+    emailElement.innerHTML = `<strong>Email:</strong> ${email}`
+
+    const itKnowledgeElement = document.createElement('p')
+    itKnowledgeElement.innerHTML = `<strong>IT Knowledge:</strong> ${itKnowledge}/10`
+
+    const groupElement = document.createElement('p')
+    groupElement.innerHTML = `<strong>Group:</strong> ${group}`
+
+    const interestsWrapperElement = document.createElement('div')
+    const interestsHeaderElement = document.createElement('h3')
+    interestsHeaderElement.textContent = 'Interests:'
+
+    const interestsList = document.createElement('ul')
+
+    interests.forEach(interest => {
+      const interestItem = document.createElement('li')
+      interestItem.textContent = interest.value
+      interestsList.append(interestItem)
+    })
+
+    interestsWrapperElement.append(interestsHeaderElement, interestsList)
+
+    studentItem.append(nameElement, ageElement, phoneElement, emailElement, itKnowledgeElement, groupElement, interestsWrapperElement)
+    studentsList.prepend(studentItem)
   })
 }
 

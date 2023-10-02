c141fd24c85b5ec1451083dd333c097349a28301
RomanasV <romanasvenckus@gmail.com>
2023-10-02 09:37:42 +0300

    async await refactored AI task

--- 2023-09-28-api/ai.js
+++ 2023-09-28-api/ai.js
@@ -2,45 +2,56 @@ import { firsLetterUppercase } from './utils.js'
 
 function init() {
   const nameForm = document.querySelector('#name-form')
-  const outputParagraph = document.querySelector('#output-paragraph')
   
-  nameForm.addEventListener('submit', event => {
+  nameForm.addEventListener('submit', async (event) => {
     event.preventDefault()
-
-    outputParagraph.textContent = ''
+    
+    const outputParagraph = document.querySelector('#output-paragraph')
 
     const enteredName = event.target.name.value
 
-    fetch('https://api.agify.io?name=' + enteredName)
-      .then(res => res.json())
-      .then(data => {
-        const age = data.age
+    const ageOutput = await getAgeByName(enteredName)
+    const countryOutput = await getCountryByName(enteredName)
+    const genderOutput = await getGenderByName(enteredName)
 
-        outputParagraph.textContent += `${firsLetterUppercase(enteredName)} is ${age} years old. `
-
-        fetch('https://api.nationalize.io?name=' + enteredName)
-          .then(res => res.json())
-          .then(data => {
-            const countryId = data.country[0].country_id
-    
-            outputParagraph.textContent += `${firsLetterUppercase(enteredName)} is  from ${countryId}. `
-
-            fetch('https://api.genderize.io/?name=' + enteredName)
-              .then(res => res.json())
-              .then(data => {
-                const gender = data.gender
-        
-                outputParagraph.textContent += `${firsLetterUppercase(enteredName)} is  a ${gender}. `
-              })
-          })
-      })
-
-
-
-
-
-      
+    outputParagraph.textContent = `${ageOutput} ${countryOutput} ${genderOutput}`
   })
 }
 
 init()
+
+async function getAgeByName(enteredName) {
+  const res = await fetch('https://api.agify.io?name=' + enteredName)
+  const data = await res.json()
+
+  const age = data.age
+  const output = `${firsLetterUppercase(enteredName)} is ${age} years old.`
+  
+  return output
+}
+
+async function getCountryByName(enteredName) {
+  const res = await fetch('https://api.nationalize.io?name=' + enteredName)
+  const data = await res.json()
+  
+  const countryId = data.country[0].country_id
+  const output = `${firsLetterUppercase(enteredName)} is from ${countryId}.`
+
+  return output
+}
+
+async function getGenderByName(enteredName) {
+  // fetch('https://api.genderize.io/?name=john')
+  //   .then(res => res.json())
+  //   .then(data => {
+  //     console.log(data)
+  //   })
+
+  const res = await fetch('https://api.genderize.io/?name=' + enteredName)
+  const data = await res.json()
+
+  const gender = data.gender
+  const output = `${firsLetterUppercase(enteredName)} is a ${gender}.`
+  
+  return output
+}
